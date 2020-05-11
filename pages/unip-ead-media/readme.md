ddef9f318f9c23d2e3cb4005c30039eea4681e74
Dan Buck <35202075+danbuck84@users.noreply.github.com>
2020-05-11 17:41:08 -0300

    fixing logo, styling and adding proper script file

---
+++ pages/unip-ead-media/readme.md
@@ -0,0 +1,10 @@
+Programinha bem básico para auxiliar os estudantes de cursos tecnológicos da UNIP EaD
+a calcular suas médias.
+
+O cálculo da média é feito da seguinte forma:
+
+Média = (7 x Prova) + (2 x PIM) + (1 x AVA) / 10
+
+Usei JQuery neste script.
+
+Maio de 2020.
---
+++ pages/unip-ead-media/script.js
@@ -0,0 +1,21 @@
+$(".txtb input").on("focus", function() {
+    $(this).addClass("focus");
+});
+
+$(".txtb input").on("blur", function() {
+    if($(this).val() == "")
+    $(this).removeClass("focus");
+});
+
+function CALC() {
+    var prova = document.getElementById("prova").value;
+    var pim = document.getElementById("pim").value;
+    var ava = document.getElementById("ava").value;
+
+    var nota = (7 * prova + 2 * pim + 1 * ava ) / 10.;
+    var arred = parseFloat(nota.toFixed(2));
+    
+    event.preventDefault();
+    
+    document.getElementById("result").innerHTML = "Sua média final foi de " + arred;
+};
