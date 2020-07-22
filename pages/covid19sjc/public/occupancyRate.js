4f2a9bf2b84e0b56f1df7cdcfebea3bb180bed0e
Dan Buck <35202075+danbuck84@users.noreply.github.com>
2020-07-22 18:42:22 -0300

    updating proper files with today's new figures

--- pages/covid19sjc/public/occupancyRate.js
+++ pages/covid19sjc/public/occupancyRate.js
@@ -45,12 +45,12 @@ async function occupancyRate() {
         datasets: [
             {
                 data: [
-                    73.7, // icu covid
-                    37.3, // icu general
-                    80, // icu hm
-                    61.4,  // ward covid
-                    10.8, // ward general
-                    63.8, // ward hm
+                    62.7, // icu covid
+                    31.5, // icu general
+                    60, // icu hm
+                    58.3,  // ward covid
+                    10.3, // ward general
+                    72.5, // ward hm
                 ],
                 backgroundColor: [
                     'rgba(192, 57, 43,0.5)',  // icu covid
